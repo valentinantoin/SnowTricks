b0eb6b984ee3b01daebb8079fd57e7b49ba38039
valentinantoin <valentin.antoin@gmail.com>
2019-10-21 13:10:12 +0200

    Add removeComment method

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use App\Entity\Tricks;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,12 +18,11 @@ class CommentController extends AbstractController
      * @Route("/createcomment/{id}", name="createComment")
      * @param Request $request
      * @param ObjectManager $manager
-     * @param Security $security
      * @param $id
      * @return Response
      * @throws \Exception
      */
-    public function createComment(Request $request, ObjectManager $manager, Security $security, $id)
+    public function createComment(Request $request, ObjectManager $manager, $id)
     {
         $comment = new Comment();
 
@@ -44,5 +44,19 @@ class CommentController extends AbstractController
 
         }
             return $this->render('home/home.html.twig');
+        }
+
+    /**
+     * @route("/removeComment/{id}", name="removeComment")
+     * @param Comment $comment
+     * @return RedirectResponse
+     */
+        public function removeComment( Comment $comment)
+        {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('account');
         }
 }
