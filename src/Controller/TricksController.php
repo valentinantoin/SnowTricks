fcb8c7992fd4e85570a781bce0696b65436091be
valentinantoin <valentin.antoin@gmail.com>
2019-10-25 14:06:35 +0200

    Add cretaTrick method

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Tricks;
+use App\Form\TrickType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -77,15 +82,61 @@ class TricksController extends AbstractController
 
     /**
      * @route("/removeTrick/{id}", name="removeTrick")
-     * @param tricks $trick
+     * @param ObjectManager $manager
+     * @param $id
      * @return RedirectResponse
      */
-    public function removeTrick( tricks $trick)
+    public function removeTrick( ObjectManager $manager, $id)
     {
-        $manager = $this->getDoctrine()->getManager();
+        $repoTricks = $this->getDoctrine()->getRepository(Tricks::class);
+        $trick = $repoTricks->find($id);
+
         $manager->remove($trick);
         $manager->flush();
 
         return $this->redirectToRoute('admin', ['_fragment' => 'tricks']);
+    }
+
+    /**
+     * @Route("/createTrick", name="createTrick")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return Response
+     * @throws \Exception
+     */
+    public function createTrick(Request $request, ObjectManager $manager)
+    {
+        $trick = new Tricks();
+        $trick->setCreatedAt(new \DateTime());
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $img = $form['img']->getData();
+
+            $originalImg = pathinfo($img->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeImg = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalImg);
+            $newImg = $safeImg.'-'.uniqid().'.'.$img->guessExtension();
+
+            try {
+                $img->move(
+                    $this->getParameter('trick_directory'),
+                    $newImg
+                );
+            } catch (FileException $e) {
+                throw new \Exception('Une erreur s\'est produite');
+            }
+
+            $trick->setImg($newImg);
+            $manager->persist($trick);
+            $manager->flush();
+
+            return $this->redirectToRoute('tricks');
+        }
+
+        return $this->render('tricks/createTrick.html.twig',[
+            'form' => $form->createView()
+        ]);
     }
 }
