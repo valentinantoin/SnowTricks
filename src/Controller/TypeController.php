5b03bfaffadcb8476e1ae865305d547a931d16c6
valentinantoin <valentin.antoin@gmail.com>
2019-10-30 20:06:24 +0100

    Create Controller & createType method

---
+++ src/Controller/TypeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Type;
+use App\Form\TypeType;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TypeController extends AbstractController
+{
+    /**
+     * @Route("/nouveauType", name="createType")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return RedirectResponse | Response
+     */
+    public function createType(Request $request, ObjectManager $manager)
+    {
+        $type = new Type();
+
+        $typeForm = $this->createForm(TypeType::class);
+        $typeForm->handleRequest($request);
+
+        if ($typeForm->isSubmitted() && $typeForm->isValid()){
+
+            $type = $typeForm->getData();
+
+            $manager->persist($type);
+            $manager->flush();
+
+            $this->addFlash('success', 'Type ajouté avec succes !');
+
+            return $this->redirectToRoute('createTrick');
+        }
+
+        return $this->render('type/createType.html.twig', [
+            'form' => $typeForm->createView()
+        ]);
+    }
+}
