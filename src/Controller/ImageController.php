eae7eb3a15eab6d56b588fbe76f6e13d4ad8e80c
valentinantoin <valentin.antoin@gmail.com>
2019-11-07 15:42:05 +0100

    Create addImage method

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -3,13 +3,44 @@
 namespace App\Controller;
 
 use App\Entity\Image;
+use App\Entity\Tricks;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ImageController extends AbstractController
 {
+    /**
+     * @Route("/addImg/{id}", name="addImg")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @param $id
+     * @return RedirectResponse
+     */
+    public function addImage(Request $request, ObjectManager $manager, $id)
+    {
+        $img = new Image();
+
+        if($request->isMethod('post')){
+            $url = $request->get("url");
+
+            $repotrick = $this->getDoctrine()->getRepository(Tricks::class);
+            $trick = $repotrick->find($id);
+
+            $img->setTrickId($trick)
+                ->setUrl($url);
+
+            $manager->persist($img);
+            $manager->flush();
+
+            return $this->redirectToRoute('trick', ['id' => $id, '_fragment' => 'media']);
+        }
+
+        return $this->redirectToRoute('trick', [ 'id' => $id]);
+    }
+
     /**
      * @Route("/deleteImg/{id}", name="deleteImg")
      * @param Image $img
