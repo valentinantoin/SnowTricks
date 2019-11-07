760ea1e1825f9c989a1406ac38f7eec69b420cbd
valentinantoin <valentin.antoin@gmail.com>
2019-11-07 14:30:14 +0100

    Create ImageController & deleteImg method

---
+++ src/Controller/ImageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Image;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ImageController extends AbstractController
+{
+    /**
+     * @Route("/deleteImg/{id}", name="deleteImg")
+     * @param Image $img
+     * @return RedirectResponse
+     */
+    public function deleteImg(Image $img)
+    {
+        $trickId = $img->getTrickId()->getId();
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($img);
+        $manager->flush();
+
+        return $this->redirectToRoute('trick', ['id' => $trickId]);
+    }
+}
